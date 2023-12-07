a2762ab184d15332cddb332f69e60aebf4768bdf
kr$hna <82963568+krishnakiller@users.noreply.github.com>
2023-12-07 21:36:19 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,7 @@
+// Change the text between the quotation marks and run your code to see the name of your favorite language animated!
+drawText('TIC TAC TOE');
+// drawText(AM);
+// drawText(KRISHNA);
+
+// The code below will allow you to interact with the letters
+bounceBubbles();
